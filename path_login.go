c194f825398869b879ab23d9bdf5be71f83958c6
ka-yamag <pupupu0701@gmail.com>
2019-09-28 22:32:27 +0900

    add athenz pkg

--- path_login.go
+++ path_login.go
@@ -1,1 +1,84 @@
 package athenzauth
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/hashicorp/vault/sdk/framework"
+	"github.com/hashicorp/vault/sdk/logical"
+)
+
+func pathLogin(b *athenzAuthBackend) *framework.Path {
+	return &framework.Path{
+		Pattern: "login",
+		Fields: map[string]*framework.FieldSchema{
+			"name": &framework.FieldSchema{
+				Type: framework.TypeString,
+			},
+			"ntoken": &framework.FieldSchema{
+				Type: framework.TypeString,
+			},
+		},
+		Callbacks: map[logical.Operation]framework.OperationFunc{
+			logical.UpdateOperation: b.pathAuthLogin,
+		},
+	}
+}
+
+func (b *athenzAuthBackend) pathAuthLogin(ctx context.Context, req *logical.Request, d *framework.FieldData) (*logical.Response, error) {
+	name := strings.ToLower(d.Get("name").(string))
+	if name == "" {
+		return nil, errors.New("missing name")
+	}
+
+	roletoken := d.Get("roletoken").(string)
+	if roletoken == "" {
+		return nil, errors.New("missing roletoken")
+	}
+
+	athenzEntry, err := b.athenz(ctx, req.Storage, name)
+	if err != nil {
+		return nil, err
+	}
+
+	// rt, err := athenz.GetUpdater().VerifyRoleToken(b.updaterCtx, roletoken)
+	// if err != nil {
+	//   return nil, logical.ErrPermissionDenied
+	// }
+
+	// // Check issue domain
+	// if athenzEntry.AthenzIssueDomain != rt.Domain {
+	//   return nil, errors.New("the issue domain contained in roletoken does not match registerd it")
+	// }
+
+	// // Check whether role token includes the registered roles
+	// count := 0
+	// for _, regRole := range athenzEntry.AthenzRoles {
+	//   for _, tokenRole := range rt.Roles {
+	//     if regRole == tokenRole {
+	//       count++
+	//     }
+	//   }
+	// }
+	// if count < len(athenzEntry.AthenzRoles) {
+	//   return nil, logical.ErrPermissionDenied
+	// }
+	return &logical.Response{
+		Auth: &logical.Auth{
+			InternalData: map[string]interface{}{
+				// "athenzIssueDomain": athenzEntry.AthenzIssueDomain,
+				// "athenzRoles":       strings.Join(athenzEntry.AthenzRoles, ", "),
+			},
+			Policies: athenzEntry.Policies,
+			Metadata: map[string]string{
+				"name": athenzEntry.Name,
+			},
+			LeaseOptions: logical.LeaseOptions{
+				TTL:       athenzEntry.TTL,
+				MaxTTL:    athenzEntry.MaxTTL,
+				Renewable: true,
+			},
+		},
+	}, nil
+}
